27be558ea37c070015e249bffd922107ce8abd33
vmustone <111465619+vmustone@users.noreply.github.com>
2023-10-27 03:41:47 +0300

    Add files via upload

---
+++ cpp4/ex03/AMateria.cpp
@@ -0,0 +1,44 @@
+
+#include "AMateria.hpp"
+
+AMateria::AMateria() {
+	std::cout << "AMateria constructor called" << std::endl;
+	_type = "";
+}
+
+AMateria::~AMateria() {
+	std::cout << "AMateria destructor called" << std::endl;
+}
+
+AMateria::AMateria(std::string const & type) {
+	std::cout << "AMateria type constructor called" << std::endl;
+	_type = type;
+}
+
+AMateria::AMateria(const AMateria& other) {
+	std::cout << "AMateria copy constructor called" << std::endl;
+	*this = other;
+}
+
+AMateria&	AMateria::operator=(const AMateria& other) {
+	std::cout << "AMateria assignation constructor called" << std::endl;
+	if (this != &other) {
+		this->_type = other._type;
+	}
+	return *this;
+}
+
+std::string const & AMateria::getType() const
+{
+    return _type;
+}
+
+AMateria* AMateria::clone() const
+{
+    return (AMateria*)this;
+}
+
+void    AMateria::use( ICharacter& target )
+{
+    std::cout << "AMateria " << _type << " used on " << target.getName() << std::endl;
+}
--- cpp4/ex03/AMateria.hpp
+++ cpp4/ex03/AMateria.hpp
@@ -3,15 +3,18 @@
 #include "ICharacter.hpp"
 #include <iostream>
 
-class AMateria
-{
+class AMateria {
 protected:
-std::string _type;
+	std::string _type;
 public:
-AMateria(std::string const & type);
-std::string const & getType() const; //Returns the materia type
-virtual AMateria* clone() const = 0;
-virtual void use(ICharacter& target);
+	AMateria();
+	virtual ~AMateria();
+	AMateria(std::string const & type);
+	AMateria(const AMateria& other);
+	AMateria&	operator=(const AMateria& other);
+	std::string const & getType() const; //Returns the materia type
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
 };
 
 #endif
